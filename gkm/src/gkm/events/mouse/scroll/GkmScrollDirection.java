e36997bdec3ef38c063adec293d1fcd7623ebb43
MiguelBCosta <miguelbertemes@hotmail.com>
2025-05-17 16:31:46 -0300

    fix scroll

--- gkm/src/gkm/events/mouse/scroll/GkmScrollDirection.java
+++ gkm/src/gkm/events/mouse/scroll/GkmScrollDirection.java
@@ -7,9 +7,9 @@ public enum GkmScrollDirection {
 
     public static GkmScrollDirection fromInt(int direction) {
         switch (direction) {
-            case 1:
-                return UP;
             case -1:
+                return UP;
+            case 1:
                 return DOWN;
             default:
                 return UNKNOWN;
